5050892ae3f8508581123aeb654f952ec40d9b15
Alfonso Alejandro Arroyo Paredes <arroyoalfonso1031@gmail.com>
2024-07-02 00:22:59 -0500

    code update v1.1

---
+++ CODIGO/InventorySystem/src/ec/edu/espe/inventorysystem/model/Product.java
@@ -0,0 +1,221 @@
+package ec.edu.espe.inventorysystem.model;
+
+import ec.edu.espe.inventorysystem.utils.ProductManager;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class Product {
+
+    private String id;
+    private String name;
+    private String description;
+    private int quantity;
+    private String category;
+    private float price;
+    private String size;
+    Scanner scanner = new Scanner(System.in);
+
+    public Product(String id, String name, String description, int quantity, String category, float price, String size) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+        this.category = category;
+        this.price = price;
+        this.size = size;
+    }
+
+    public float calculateTotalSale() {
+        float totalSale = getQuantity() * getPrice();
+        System.out.println("Total sale: $" + totalSale);
+        return totalSale;
+    }
+
+    public void checkTotalOfProducts() {
+        int productCount = ProductManager.getProductCount();
+        System.out.println("Total number of products: " + productCount);
+    }
+
+     public  void addProduct() {
+        try {
+            System.out.print("Id: ");
+            String id = readAlphanumericInput();
+
+            System.out.print("Name: ");
+            String name = readLettersWithSpacesInput();
+
+            System.out.print("Description: ");
+            String description = readLettersWithSpacesInput();
+
+            System.out.print("Quantity: ");
+            int quantity = readIntegerInput();
+
+            System.out.print("Category: ");
+            String category = scanner.nextLine();
+
+            System.out.print("Price: ");
+            float price = readFloatInput();
+
+            System.out.print("Size: ");
+            String size = readNonEmptyStringInput();
+
+            ProductManager.saveProduct(id, name, description, quantity, category, price, size);
+            System.out.println("Product Saved");
+        } catch (InputMismatchException e) {
+            System.err.println(e.getMessage());
+            scanner.nextLine();  // Limpiar el búfer del escáner
+        }
+    }
+     private  String readAlphanumericInput() {
+        while (true) {
+            String input = scanner.nextLine().trim();
+            if (input.matches("^[a-zA-Z0-9]+$")) {
+                return input;
+            } else {
+                System.err.println("Invalid input. Please enter alphanumeric characters only.");
+            }
+        }
+    }
+
+    private  String readLettersWithSpacesInput() {
+        while (true) {
+            String input = scanner.nextLine().trim();
+            if (input.matches("^[a-zA-Z ]+$")) {
+                return input;
+            } else {
+                System.err.println("Invalid input. Please enter letters and spaces only.");
+            }
+        }
+    }
+
+    private  int readIntegerInput() {
+        while (true) {
+            try {
+                return scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.err.println("Invalid input. Please enter an integer.");
+                scanner.nextLine();  // Limpiar el búfer del escáner
+            }
+        }
+    }
+
+    private  float readFloatInput() {
+        while (true) {
+            try {
+                return scanner.nextFloat();
+            } catch (InputMismatchException e) {
+                System.err.println("Invalid input. Please enter a valid float number.");
+                scanner.nextLine();  // Limpiar el búfer del escáner
+            }
+        }
+    }
+
+    private  String readNonEmptyStringInput() {
+        while (true) {
+            String input = scanner.nextLine().trim();
+            if (!input.isEmpty()) {
+                return input;
+            } else {
+                System.err.println("Input cannot be empty. Please enter again.");
+            }
+        }
+    }
+
+
+    public void removeProduct() {
+        System.out.print("Enter ID of the product to remove: ");
+        String idToRemove = scanner.nextLine();
+        ProductManager.removeProduct(idToRemove);
+    }
+
+    public void editProduct() {
+        try {
+           ProductManager.editProduct();
+            
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a valid digit.");
+            scanner.nextLine();
+        }
+    }
+    
+     public void editQuantity() {
+        Scanner scanner = new Scanner(System.in);
+        ProductManager productManager = new ProductManager();
+        
+        try {
+            System.out.print("Enter product ID to add quantity: ");
+            String productId = scanner.nextLine();
+
+            System.out.print("Enter quantity to add: ");
+            int quantityToAdd = Integer.parseInt(scanner.nextLine());
+
+            productManager.addQuantityToProduct(productId, quantityToAdd);
+        } catch (NumberFormatException e) {
+            System.err.println("Error: Please enter a valid number for quantity.");
+        }
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Product id: " + getId() + " name: " + getName() + " description: " + getDescription() + " quantity: " + getQuantity() + " category: " + getCategory() + " price: " + getPrice() + " size: " + getSize();
+    }
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
---
+++ CODIGO/InventorySystem/src/ec/edu/espe/inventorysystem/model/UserInterface.java
@@ -0,0 +1,93 @@
+package ec.edu.espe.inventorysystem.model;
+
+import ec.edu.espe.inventorysystem.utils.InvoiceManager;
+import java.text.ParseException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+import ec.edu.espe.inventorysystem.utils.ProductManager;
+
+/**
+ * @author <As-Byte Wizards> DDCC ESPE
+ */
+public class UserInterface {
+
+    int option = 0;
+
+    Scanner scanner = new Scanner(System.in);
+    ManagementSystem userMana = new ManagementSystem();
+    Product product = new Product("", "", "", 0, "", 0, "size");
+    
+    public void displayMenuAdmin() throws ParseException {
+
+        do {
+            try {
+                System.out.println("\n\n-----------------------------------");
+                System.out.println("|      Inventory System Menu      |");
+                System.out.println("-----------------------------------");
+        
+                System.out.println("|        [1] Add product          |");
+                System.out.println("|        [2] Remove product       |");
+                System.out.println("|        [3] Update product       |");
+                System.out.println("|        [4] View all products    |");
+                System.out.println("|        [5] Edit Quantity        |");
+                System.out.println("|        [6] Change User          |");
+                System.out.println("|        [7] Create Invoice       |");
+                System.out.println("|        [8] Prinf Invoice        |");     
+                System.out.println("|        [9] Delate Invoice       |");
+                System.out.println("|        [10] Exit                 |\n");
+                System.out.print("Enter your option: ");
+                option = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (option) {
+                    case 1:
+                        product.addProduct();
+                        break;
+                    case 2:
+                        product.removeProduct();
+                        break;
+                    case 3:
+                        product.editProduct();
+                        break;
+                    case 4:
+                        ProductManager.readProducts();
+                        break;
+                    case 5:
+                        product.editQuantity();
+                        break;
+                    case 6:
+                        userMana.logIn();
+                        break;
+                    case 7:
+                        InvoiceManager.createInvoice();
+                        break;
+                    case 8:
+                        InvoiceManager.displayInvoiceList();
+                        System.out.println("Enter the invoice number you want to view");
+                        int numberInvoice = scanner.nextInt();
+                        InvoiceManager.viewInvoice(numberInvoice-1);
+                        break;
+                    case 9:
+                        InvoiceManager.displayInvoiceList();
+                        System.out.println("Enter the invoice delate");
+                        int numberInvoiceDelate = scanner.nextInt();
+                        InvoiceManager.deleteInvoice(numberInvoiceDelate-1);
+   
+                        break;
+                    case 10:
+                        userMana.logout();
+                        System.exit(0);
+                        break;
+                    default:
+                        System.err.println("Invalid option. Please try again.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid option.");
+                scanner.nextLine();
+            }
+        } while (option!=10);
+    }
+
+}
